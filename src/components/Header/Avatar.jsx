cca50777f419694445a1260701bb98867bc6c38d
João Gabriel <joaogabriel9633@gmail.com>
2023-06-25 10:46:26 -0300

    first commit

---
+++ src/components/Header/Avatar.jsx
@@ -0,0 +1,11 @@
+export default function Avatar(){
+    return (
+        <img className="rounded-full border-2 border-zinc-400
+        "
+            width={40}
+            height={40}
+            src="/use.svg" 
+            alt="user" 
+        />
+    )
+}
